d6638f2560a3f1bdb62ccc89e2d498f351012ea3
Dirtjam <107303575+Dirtjam@users.noreply.github.com>
2025-03-09 14:50:11 -0400

    idk but telescope works now

--- lua/plugins/custom/lazygit.lua
+++ lua/plugins/custom/lazygit.lua
@@ -1,28 +1,28 @@
 -- nvim v0.8.0
 return {
-    "kdheepak/lazygit.nvim",
-    -- lazy = true,
-    event = { 'BufReadPre', 'BufNewFile' },
-    cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        {
-            "nvim-telescope/telescope.nvim",
-            tag = "0.1.8",
-        },
-    },
-    config = function()
-        require("telescope").load_extension("lazygit")
+	"kdheepak/lazygit.nvim",
+	-- lazy = true,
+	event = { "BufReadPre", "BufNewFile" },
+	cmd = {
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFilterCurrentFile",
+	},
+	-- optional for floating window border decoration
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope.nvim",
+			tag = "0.1.8",
+		},
+	},
+	config = function()
+		require("telescope").load_extension("lazygit")
 
-        require("which-key").add({
-            {"<leader>hg", "<cmd>LazyGit<cr>", desc = "Lazy[G]it"}
-        })
-    end,
+		require("which-key").add({
+			{ "<leader>hg", "<cmd>LazyGit<cr>", desc = "Lazy[G]it" },
+		})
+	end,
 }
