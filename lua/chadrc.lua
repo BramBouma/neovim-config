e531d68f682d74572bf02b2b921694d99a8438fa
Dirtjam <107303575+Dirtjam@users.noreply.github.com>
2025-06-11 22:28:55 -0400

    chadrc init

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -3,6 +3,16 @@ local M = {
 		theme = { "onedark" },
 		transparency = true,
 	},
+	ui = {
+		telescope = { style = "bordered" },
+	},
+	statusline = {
+		-- options: default, vscode, vscode_colored, minimal
+		theme = "default",
+
+		-- options: default, round, block, arrow (some only work with default and/or minimal theme only)
+		style = "arrow",
+	},
 }
 
 return M
