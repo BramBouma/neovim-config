171880edeb666d73e1eb81ad27d808884f9771fa
Dirtjam <107303575+Dirtjam@users.noreply.github.com>
2025-06-13 15:31:24 -0400

    nvdash and minty

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,6 +1,6 @@
 local M = {
 	base46 = {
-		theme = { "ayu_dark" },
+		theme = "ayu_dark",
 		transparency = true,
 	},
 	ui = {
@@ -13,12 +13,46 @@ local M = {
 	-- 	-- options: default, round, block, arrow (some only work with default and/or minimal theme only)
 	-- 	style = "arrow",
 	-- },
-	-- tabufline = {
-	-- 	lazyload = false,
-	-- },
-	-- nvdash = {
-	-- 	load_on_startup = true,
-	-- },
+	tabufline = {
+		lazyload = false,
+	},
+	nvdash = {
+		load_on_startup = true,
+		header = {
+
+			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗  ",
+			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║  ",
+			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║  ",
+			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║  ",
+			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║  ",
+			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝  ",
+			"                                                      ",
+			"                                                      ",
+		},
+		buttons = {
+			{ txt = "  Find File", keys = "f", cmd = "Telescope find_files" },
+			{ txt = "  Recent Files", keys = "r", cmd = "Telescope oldfiles" },
+			{ txt = "󰈭  Live Grep", keys = "s", cmd = "Telescope live_grep" },
+			{ txt = "󰊢  LazyGit", keys = "g", cmd = "LazyGit" },
+			{ txt = "  Lazy", keys = "l", cmd = "Lazy" },
+			{ txt = "󰒋  Mason", keys = "m", cmd = "Mason" },
+			{ txt = "  Quit", keys = "q", cmd = "qa" },
+
+			{ txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+
+			{
+				txt = function()
+					local stats = require("lazy").stats()
+					local ms = math.floor(stats.startuptime) .. " ms"
+					return "Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+				end,
+				hl = "NvDashFooter",
+				no_gap = true,
+			},
+
+			{ txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+		},
+	},
 }
 
 return M
