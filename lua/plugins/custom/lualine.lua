83c6f7537cf82dbcb718018cea47f611b9ed3757
Dirtjam <107303575+Dirtjam@users.noreply.github.com>
2025-07-21 17:33:19 -0400

    idk

--- lua/plugins/custom/lualine.lua
+++ lua/plugins/custom/lualine.lua
@@ -1,16 +1,17 @@
 return {
 	"nvim-lualine/lualine.nvim",
+	enabled = false,
 	event = "VeryLazy",
 	dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
 	init = function()
-		vim.g.lualine_laststatus = vim.o.laststatus
-		if vim.fn.argc(-1) > 0 then
-			-- set an empty statusline till lualine loads
-			vim.o.statusline = " "
-		else
-			-- hide the statusline on the starter page
-			vim.o.laststatus = 0
-		end
+		-- vim.g.lualine_laststatus = vim.o.laststatus
+		-- if vim.fn.argc(-1) > 0 then
+		-- 	-- set an empty statusline till lualine loads
+		-- 	vim.o.statusline = " "
+		-- else
+		-- 	-- hide the statusline on the starter page
+		-- 	vim.o.laststatus = 0
+		-- end
 	end,
 	-- opts = function()
 	-- 	-- PERF: we don't need this lualine require madness 🤷
